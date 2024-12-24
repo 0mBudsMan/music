0836f1c6fe6731563ffb32ac04eb551930469f30
0mBudsMan <buddhadevom@gmail.com>
2024-12-24 05:45:06 +0530

    some routes

---
+++ src/pages/api/downvotes/route.js
@@ -0,0 +1,48 @@
+import { getServerSession } from "next-auth";
+import { PrismaClient } from "../../../lib/db";
+import { useSearchParams } from 'next/navigation'
+
+export async function post(req, res) {
+  const session = await getServerSession();
+  if (session?.user?.email) {
+    return res.status(403).json({
+      message: "Unautheniticated",
+    });
+  }
+  const userid = await PrismaClient.user.findFirst({
+    where: {
+      email: session?.user?.email,
+    },
+  });
+  if (!userid) {
+    return res.status(403).json({
+      message: "User with such email id doesnt exist",
+    });
+  }
+  try {
+    const data = await req.json();
+    await PrismaClient.upvote.delete({
+      where: {
+        userId: userid,
+        streamId: data.streamId,
+      },
+    });
+  } catch (e) {
+    return res.statuss(403).json({
+      message: "Erorr",
+    });
+  }
+}
+
+export async function get(req,res){
+    const searchParams = useSearchParams().get("creatorId");
+    const streams = await PrismaClient.stream.findMany({
+        where:{
+            userId: creatorId
+        }
+    })
+    return res.json({
+        streams
+    })
+    
+}
--- src/pages/api/streams/route.js
+++ src/pages/api/streams/route.js
@@ -1,6 +1,20 @@
+import {PrismaClient} from "../../../lib/db"
 export async function post(req,res){
     try{
         const data = await req.json();
+        const YTR = new RegExp("^(https?:\\/\\/)?(www\\.)?(youtube\\.com\\/(watch\\?v=|embed\\/|v\\/)|youtu\\.be\\/)([\\w\\-]{11})([&?=\\w]*)?$");
+        const isYT = YTR.test(data.url);
+        if(!isYT){
+            return res.status(411).json({
+                message: "Error valid URL"
+            })
+        }
+        const extractedID = data.url.split("?v=")[1];
+        await PrismaClient.stream.create({
+            userId: data.creatorID,
+            url: data.url,
+            extractedID
+        })
     }
     catch(e){
         return res.status(411).json({
